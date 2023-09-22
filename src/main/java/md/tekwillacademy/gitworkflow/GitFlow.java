a5586694587abd55bb7c13c6c42f16798bf73d2b
angelachihii <chihiiangela@gmail.com>
2023-09-22 20:35:34 +1300

    added git work flow

---
+++ src/main/java/md/tekwillacademy/gitworkflow/GitFlow.java
@@ -0,0 +1,43 @@
+package md.tekwillacademy.gitworkflow;
+
+public class GitFlow {
+    public static void main(String[] args) {
+        String step1 ="Perform git stash - to store the uncomitted changes";
+        String step2 = "Perform git cheeckout master - to move to the local master branch";
+        String step3 = "Perform git fetch command - to fetch the origin branches";
+        String step4 = "Perform git cheeckout -b new_branch_name command - to create and instantly move to a new branch,";
+        String step5 = "Add you changes that related to the task";
+        String step6 = "Review your changes and check your code before committing them";
+        String step7 = "Execute git commit by using the Intellij interface, where you want to commit";
+        String step8 = "Once all the changes, are commited , execute a git push command by using CTRL+ALT+K hot keys";
+        String step9 = "The coomitted changes are pushed to an origin branch with the same name, navigate to the GitHub branches";
+        String step10 = "Create a pull Request from origin/new_branch_name into origin/master";
+        String step11 = "Add the Pull Request details - name, description and request review from your coll";
+        String step12 = "If you collest at last 2 approvals from your colleagues than morg the branch";
+        String step13 = "If a change is requared , then the last commit will be adjusted by using git commit amend";
+        String step14 = "In a acse of commid amend then a git force will be performed in oreder to update the remote branch";
+        String step15 = "After that , the origin/master contains the latest changes and the origin/new_branch_name can be deleted";
+        String step16 = "Do not forget that locally you are still on the new_branch_name, it's good to move back to master";
+        String step17 = "However , the local master branch does not have the latest changes, therefore perform a git fetch and git pull";
+
+        System.out.println(step1);
+        System.out.println(step2);
+        System.out.println(step3);
+        System.out.println(step4);
+        System.out.println(step5);
+        System.out.println(step6);
+        System.out.println(step7);
+        System.out.println(step8);
+        System.out.println(step9);
+        System.out.println(step10);
+        System.out.println(step11);
+        System.out.println(step12);
+        System.out.println(step13);
+        System.out.println(step14);
+        System.out.println(step15);
+        System.out.println(step16);
+        System.out.println(step17);
+
+
+    }
+}
